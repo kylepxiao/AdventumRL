8e74d1ec82c7751c889a74f3d62f6897b95f9016
Pr-Kumar <41289983+Pr-Kumar@users.noreply.github.com>
2020-10-15 10:55:31 -0400

    Setting up local repo by adding commented files and readme (#2)

--- Grammar_Demo/models/DQNAgent.py
+++ Grammar_Demo/models/DQNAgent.py
@@ -28,32 +28,37 @@ class DQNAgent(Agent):
     """Deep Q-learning agent for discrete state/action spaces."""
 
     def __init__(self, agentHost=None):
+        # set debuggers
         self.logger = logging.getLogger(__name__)
-        if False: # True if you want to see more information
+        self.verbose = agentHost.verbose
+        if self.verbose: # True if you want to see more information
             self.logger.setLevel(logging.DEBUG)
-        else:
-            self.logger.setLevel(logging.INFO)
         self.logger.handlers = []
         self.logger.addHandler(logging.StreamHandler(sys.stdout))
 
+        # set host based on MalmoLogicState.py
         self.host = agentHost
         logicState = agentHost.state
         self.move_actions = ["movenorth 1", "movesouth 1", "movewest 1", "moveeast 1"]
         (x1, y1, z1), (x2, y2, z2) = logicState.world_bounds.roundPosition()
+
+        # set dqn neural net in DeepQLearner.py
         self.learner = DeepQLearner(
             input_size= (x2-x1+1) + (z2-z1+1)
                 + len(logicState.actions) + len(logicState.triggers),
             num_actions=len(self.move_actions) + len(logicState.actions),
             load_path='cache/dqn.pkl',
             save_path='cache/dqn.pkl',
-            verbose=False)
+            verbose=self.verbose)
 
+        # set vars for draw_QTable method
         self.canvas = None
         self.root = None
-        self.gamma = 0.9
+
+        # set vars for collecting output and logging numbers in logs folder
         self.cumulative_rewards = []
         tstr = time.strftime("%Y%m%d-%H%M%S")
-        self.logFile = 'DQNAgent-' + tstr + '.txt'
+        self.logFile = 'DQNAgent_Rewards-' + tstr + '.txt'
         self.lossFile = 'DQNAgent_Losses-' + tstr + '.txt'
 
     def updateGrammar(self, agentHost):
@@ -62,102 +67,114 @@ class DQNAgent(Agent):
     def getActionSpace(self):
         return self.host.getApplicableActions()
 
-    def getObservations(self):
-        return json.loads(self.host.state.observations[-1].text)
+    def getObservations(self, world_state):
+        obs_text = world_state.observations[-1].text
+        obs = json.loads(obs_text)
+        if not u'XPos' in obs or not u'ZPos' in obs:
+            raise RuntimeError("Incomplete observation received: %s" % obs_text)
+        else:
+            return obs
 
     def addObservations(self, observation):
         observation = observation or self.host.state
         self.host.updateLogicState(observation)
-
-    def queryActions(self, world_state, current_r ):
-        """take 1 action in response to the current world state"""
-
-        self.host.updateLogicState(world_state)
-        obs_text = world_state.observations[-1].text
-        obs = json.loads(obs_text) # most recent observation
-        self.logger.debug(obs)
-        if not u'XPos' in obs or not u'ZPos' in obs:
-            self.logger.error("Incomplete observation received: %s" % obs_text)
-            return 0
-
-        current_s = self.host.state.getStateEmbedding()
-        logicalActions = self.host.state.getApplicableActions()
-        actions = self.move_actions + logicalActions
-        self.logger.debug("State: %s (x = %.2f, z = %.2f)" % (current_s, float(obs[u'XPos']), float(obs[u'ZPos'])))
-
-        # update Q values
-        if self.prev_s is not None and self.prev_a is not None:
-            a = self.learner.query(current_s, current_r)
-            self.learner.run_dyna()
-        else:
-            a = self.learner.querysetstate(current_s)
-
-        #self.drawQ( curr_x = int(obs[u'XPos']), curr_y = int(obs[u'ZPos']) )
-
-        self.logger.info("Taking q action: %s" % actions[a % len(actions)])
-
-        # try to send the selected action, only update prev_s if this succeeds
-        try:
-            self.host.sendCommand(actions[a % len(actions)], is_logical = a % len(actions) >= len(self.move_actions))
-            self.prev_s = current_s
-            self.prev_a = a
-
-        except RuntimeError as e:
-            self.logger.error("Failed to send command: %s" % e)
-
-        return actions[a], current_r
-
+ 
     def setState(self, world_state):
         self.host.updateLogicState(world_state)
+
+    def getState(self):
+        return self.host.state.getStateEmbedding()
 
     def train(self):
         raise NotImplementedError
 
-    def act(self, world_state, current_r ):
-        """take 1 action in response to the current world state"""
+    def queryActions(self, world_state, current_reward):
+        """
+        Take 1 action in response to the current world state
+        Same as act method but also returns action taken
+        Required to implement because of Agent definition
+        """
 
-        self.host.updateLogicState(world_state)
-        obs_text = world_state.observations[-1].text
-        obs = json.loads(obs_text) # most recent observation
-        self.logger.debug(obs)
-        if not u'XPos' in obs or not u'ZPos' in obs:
-            self.logger.error("Incomplete observation received: %s" % obs_text)
+        # Set malmo system to current world state
+        self.setState(world_state)
+        try:
+            obs = self.getObservations(world_state) # get and print most recent observation
+        except RuntimeError as e:
+            self.logger.error(e)
             return 0
+        self.logger.debug("\n Full Observation: " + str(obs))
 
-        current_s = self.host.state.getStateEmbedding()
-        logicalActions = self.host.state.getApplicableActions()
-        actions = self.move_actions + logicalActions
-        self.logger.debug("State: %s (x = %.2f, z = %.2f)" % (current_s, float(obs[u'XPos']), float(obs[u'ZPos'])))
+
+        current_state = self.getState()
+        allActions = self.move_actions + self.getActionSpace()
+        self.logger.debug("\n State: %s (x = %.2f, z = %.2f)" % (current_state, float(obs[u'XPos']), float(obs[u'ZPos'])))
 
         # update Q values
-        if self.prev_s is not None and self.prev_a is not None:
-            a = self.learner.query(current_s, current_r)
-            self.learner.run_dyna()
+        if self.prev_state is None or self.prev_action is None:
+            action = self.learner.querysetstate(current_state)
         else:
-            a = self.learner.querysetstate(current_s)
+            action = self.learner.query(current_state, current_reward)
+            self.learner.run_dyna()
 
-        #self.drawQ( curr_x = int(obs[u'XPos']), curr_y = int(obs[u'ZPos']) )
+        if self.verbose: self.draw_QTable( curr_x = int(obs[u'XPos']), curr_y = int(obs[u'ZPos']) )
 
-        self.logger.info("Taking q action: %s" % actions[a % len(actions)])
+        self.logger.info("\n Taking q action: %s" % allActions[action % len(allActions)])
 
         # try to send the selected action, only update prev_s if this succeeds
         try:
-            self.host.sendCommand(actions[a % len(actions)], is_logical = a % len(actions) >= len(self.move_actions))
-            self.prev_s = current_s
-            self.prev_a = a
-
+            self.host.sendCommand(allActions[action % len(allActions)], is_logical = action % len(allActions) >= len(self.move_actions))
+            self.prev_state = current_state
+            self.prev_action = action
         except RuntimeError as e:
-            self.logger.error("Failed to send command: %s" % e)
+            self.logger.error("\n Failed to send command: %s" % e)
 
-        return current_r
+        return allActions[action], current_reward
+
+    def act(self, world_state, current_reward):
+        """take 1 action in response to the current world state"""
+
+        # Set malmo system to current world state
+        self.setState(world_state)
+        try:
+            obs = self.getObservations(world_state) # get and print most recent observation
+        except RuntimeError as e:
+            self.logger.error(e)
+            return 0
+        self.logger.debug("\n Full Observation: " + str(obs))
+
+        # Get state and all possible actions
+        current_state = self.host.state.getStateEmbedding()
+        logicalActions = self.host.state.getApplicableActions()
+        allActions = self.move_actions + logicalActions
+        self.logger.debug("\n State: %s (x = %.2f, z = %.2f)" % (current_state, float(obs[u'XPos']), float(obs[u'ZPos'])))
+
+        # update Q values and run neural net
+        if self.prev_state is None or self.prev_action is None:
+            action = self.learner.querysetstate(current_state)
+        else:
+            action = self.learner.query(current_state, current_reward)
+            self.learner.run_dyna()
+
+        if self.verbose: self.draw_QTable( curr_x = int(obs[u'XPos']), curr_y = int(obs[u'ZPos']) )
+        self.logger.info("\n Taking q action: %s" % allActions[action % len(allActions)])
+
+        # try to send the selected action, only update prev_s if this succeeds
+        try:
+            self.host.sendCommand(allActions[action % len(allActions)], is_logical = action % len(allActions) >= len(self.move_actions))
+            self.prev_state = current_state
+            self.prev_action = action
+        except RuntimeError as e:
+            self.logger.error("\n Failed to send command: %s" % e)
+
+        return current_reward
 
     def run(self):
         """run the agent on the world"""
 
         total_reward = 0
 
-        self.prev_s = None
-        self.prev_a = None
+        self.prev_state = None
+        self.prev_action = None
 
         is_first_action = True
 
@@ -165,7 +182,7 @@ class DQNAgent(Agent):
         world_state = self.host.getWorldState()
         while world_state.is_mission_running:
 
-            current_r = 0
+            current_reward = 0
 
             if is_first_action:
                 self.host.resetState()
@@ -175,25 +192,28 @@ class DQNAgent(Agent):
                     world_state = self.host.getWorldState()
                     for error in world_state.errors:
                         self.logger.error("Error: %s" % error.text)
+                    #accumulate reward for current state
                     for reward in world_state.rewards:
-                        current_r += reward.getValue()
-                    current_r += self.host.rewardValue()
+                        current_reward += reward.getValue()
+                    current_reward += self.host.rewardValue()
+                    # if running and have observations, act on state and set total reward to curr reward
                     if world_state.is_mission_running and len(world_state.observations)>0 and not world_state.observations[-1].text=="{}":
-                        total_reward += self.act(world_state, current_r)
+                        total_reward += self.act(world_state, current_reward)
                         break
                     if not world_state.is_mission_running:
                         break
                 is_first_action = False
             else:
                 # wait for non-zero reward
-                while world_state.is_mission_running and current_r == 0:
+                while world_state.is_mission_running and current_reward == 0:
                     time.sleep(0.1)
                     world_state = self.host.getWorldState()
                     for error in world_state.errors:
                         self.logger.error("Error: %s" % error.text)
                     for reward in world_state.rewards:
-                        current_r += reward.getValue()
-                    current_r += self.host.rewardValue()
+                        current_reward += reward.getValue()
+                    current_reward += self.host.rewardValue()
+
                 # allow time to stabilise after action
                 while True:
                     time.sleep(0.1)
@@ -201,28 +221,29 @@ class DQNAgent(Agent):
                     for error in world_state.errors:
                         self.logger.error("Error: %s" % error.text)
                     for reward in world_state.rewards:
-                        current_r += reward.getValue()
-                    current_r += self.host.rewardValue()
+                        current_reward += reward.getValue()
+                    current_reward += self.host.rewardValue()
+                    # if running and have observations, act on state and set total reward to curr reward
                     if world_state.is_mission_running and len(world_state.observations)>0 and not world_state.observations[-1].text=="{}":
-                        total_reward += self.act(world_state, current_r)
+                        total_reward += self.act(world_state, current_reward)
                         break
                     if not world_state.is_mission_running:
                         break
 
         # process final reward
-        self.logger.debug("Final reward: %d" % current_r)
-        total_reward += current_r
+        self.logger.debug("Final reward: %d" % current_reward)
+        total_reward += current_reward
 
         # update Q values
-        if self.prev_s is not None and self.prev_a is not None:
-            self.learner.query( self.host.state.getStateEmbedding(), current_r )
+        if self.prev_state is not None and self.prev_action is not None:
+            self.learner.query(self.host.state.getStateEmbedding(), current_reward)
 
-        #self.drawQ()
+        if self.verbose: self.draw_QTable()
         self.cumulative_rewards.append(total_reward)
 
         return total_reward
 
-    def drawQ( self, curr_x=None, curr_y=None ):
+    def draw_QTable( self, curr_x=None, curr_y=None ):
         scale = 40
         world_x = 6
         world_y = 14
@@ -272,98 +293,10 @@ class DQNAgent(Agent):
         self.root.update()
 
     def logOutput(self):
-        self.learner.save()
+        # self.learner.save()
         with open(os.path.join('logs', self.logFile), 'w') as f:
             for item in self.cumulative_rewards:
                 f.write("%s\n" % item)
         with open(os.path.join('logs', self.lossFile), 'w') as f:
             for item in self.learner.losses:
                 f.write("%s\n" % item)
-
-if __name__ == "__main__":
-    if sys.version_info[0] == 2:
-        sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-    else:
-        import functools
-        print = functools.partial(print, flush=True)
-
-    mission_file = './grammar_demo.xml'
-    quest_file = './quest_entities.xml'
-    agent = DQNAgent(mission_file, quest_file)
-    try:
-        agent.host.parse( sys.argv )
-    except RuntimeError as e:
-        print('ERROR:',e)
-        print(agent.host.getUsage())
-        exit(1)
-    if agent.host.receivedArgument("help"):
-        print(agent.host.getUsage())
-        exit(0)
-
-    # -- set up the mission -- #
-    with open(mission_file, 'r') as f:
-        print("Loading mission from %s" % mission_file)
-        mission_xml = f.read()
-        my_mission = MalmoPython.MissionSpec(mission_xml, True)
-    # add 20% holes for interest
-    """for x in range(1,4):
-        for z in range(1,13):
-            if random.random()<0.1:
-                my_mission.drawBlock( x,45,z,"lava")"""
-
-    max_retries = 3
-
-    checkpoint_iter = 100
-
-    if agent.host.receivedArgument("test"):
-        num_repeats = 1
-    else:
-        num_repeats = 150000
-
-    cumulative_rewards = []
-    for i in range(num_repeats):
-
-        print()
-        print('Repeat %d of %d' % ( i+1, num_repeats ))
-
-        my_mission_record = MalmoPython.MissionRecordSpec()
-
-        for retry in range(max_retries):
-            try:
-                agent.host.startMission( my_mission, my_mission_record )
-                break
-            except RuntimeError as e:
-                if retry == max_retries - 1:
-                    print("Error starting mission:",e)
-                    exit(1)
-                else:
-                    time.sleep(2.5)
-
-        print("Waiting for the mission to start", end=' ')
-        world_state = agent.host.getWorldState()
-        while not world_state.has_mission_begun:
-            print(".", end="")
-            time.sleep(0.1)
-            world_state = agent.host.getWorldState()
-            for error in world_state.errors:
-                print("Error:",error.text)
-        print()
-
-        # -- run the agent in the world -- #
-        cumulative_reward = agent.run()
-        print('Cumulative reward: %d' % cumulative_reward)
-        #cumulative_rewards += [ cumulative_reward ]
-
-        if i % checkpoint_iter == 0:
-            agent.learner.save()
-
-        agent.logOutput()
-
-        # -- clean up -- #
-        time.sleep(0.5) # (let the Mod reset)
-
-    print("Done.")
-
-    print()
-    print("Cumulative rewards for all %d runs:" % num_repeats)
-    print(self.cumulative_rewards)
